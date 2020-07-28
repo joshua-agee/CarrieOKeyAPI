5ba64024ef0dc8e8fa821d72f9428a18dbf4277b
Trung Pham <phamanhtrung@gmail.com>
2020-07-28 00:34:20 -0500

    add post route for finding song based on keyword

--- tokens.js
+++ tokens.js
@@ -22,8 +22,6 @@ const chatToken = (identity, config) => {
 };
 
 const videoToken = (identity, room, config) => {
-  console.log("access video token");
-  console.log("config: ", config);
   let videoGrant;
   if (typeof room !== "undefined") {
     videoGrant = new VideoGrant({ room });
